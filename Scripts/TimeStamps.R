621e4b9daae7371ded3ea5fb16d05a5f4e25eefc
ncondits3 <ncondits3@gatech.edu>
2023-10-05 17:25:25 -0400

    Generated files with new timestamps.

--- Scripts/TimeStamps.R
+++ Scripts/TimeStamps.R
@@ -13,7 +13,7 @@ sampleData <- sampleData[paste0(FileName, '.hum') %in% dir('Humdrum/CompleteTran
 sampleData$MirexFile <- mirexfiles[match(sampleData$ID, mirexid)]
 
 
-generateTS <- function(humfile, mirexfile) {
+generateTS <- function(humfile, mirexfile, file) {
   ###
    hummat <- stringi::stri_list2matrix(strsplit(humfile, split = '\t')) |> t()
    humchord <- hummat[ , hummat[grepl('^\\*\\*', humfile), ] == '**chordsym']
@@ -32,17 +32,34 @@ generateTS <- function(humfile, mirexfile) {
    hum <- data.table(Time = humtime, Chord = humchord, Slate = humslate)
    
    ###
+   mirexfile <- mirexfile[!grepl('^X', mirexfile)]
    mirexmat <- stringi::stri_list2matrix(strsplit(mirexfile, split = '\t')) |> t()
 
    mirexchord <- mirexmat[, 3]
-   mirexchord <- gsub('/.*', '', mirexchord)
+   mirexchord <- gsub('/[^_]*(_*)', '\\1', mirexchord)
    mirexchord[mirexchord == 'X'] <- 'N'
    mirexchord <- gsub('b:', '-:', mirexchord)
    mir <- data.table(Time =  round(as.numeric(mirexmat[, 1]), 3), Chord = mirexchord)
    if (is.na(tail(mir$Time, 1) )) mir$Time[nrow(mir)] <- round(as.numeric(mirexmat[nrow(mir) - 1,2]), 3) # last timestamp is from "end" column
    
    #
-   mir$Spans <- cumsum(mir$Time %in% humtime) 
+   
+   if (!file %in% c("SimonAndGarfunkel_ElCondorPasa_1970", "JohnnyHorton_TheBattleOfNewOrleans_1959" )) {
+      humfirstchord <- which(!is.na(hum$Chord) & !hum$Chord %in% c('r', 'N'))[1]
+      mirfirstchord <- which(mir$Chord != 'N')[1]
+      humfirsttime <- hum$Time[humfirstchord]
+      mirfirsttime <- mir$Time[mirfirstchord]
+      if (!is.na(humfirsttime) && chordsMatch(hum$Chord[humfirstchord], mir$Chord[mirfirstchord])$Match && abs(humfirsttime- mirfirsttime) < 10 && abs(humfirsttime- mirfirsttime) > 0) {
+         
+         cat('\tChanging first timestamp in .hum file by', humfirsttime - mirfirsttime,'\n')
+         hum$Time[humfirstchord] <- mir$Time[mirfirstchord]
+         
+         
+      }
+   }
+   
+   #
+   mir$Spans <- cumsum(mir$Time %in% hum$Time)
    hum$Spans <- cumsum(!is.na(hum$Time)) 
    hum$NewTime <- NA
    hum$NewChord <- NA
@@ -50,9 +67,9 @@ generateTS <- function(humfile, mirexfile) {
    lapply(union(hum$Spans, mir$Spans), 
           \(span) {
              .hum <- hum[Spans == span]
-             
-             
              .mir <- mir[Spans == span]
+             
+             if (inspect) browser()
              if (nrow(.mir) == 0) {
                 .hum$NewTime <- .hum$NewChord <- NA
                 .hum$Extra <- ''
@@ -67,33 +84,47 @@ generateTS <- function(humfile, mirexfile) {
              .hum$NewChord[targetchords] <- .mir$Chord[seq_len(ntargets)]
              
              .hum$ExtraLength <- nrow(.mir) - ntargets
-             .hum$Extra <- tail(.mir, -ntargets)[ , paste(Chord, collapse = ',')]
+             .hum$Extra <- tail(.mir, -ntargets)[ , substr(paste(Chord, collapse = ','), 0, 20)]
              .hum
           }) |> do.call(what = 'rbind') -> hum
    
    
-   
    hum <- cbind(hum, hum[, chordsMatch(Chord, NewChord)])
    
-   
-        
+   hum <- blankend(hum, file == 'StephanieMills_NeverKnewLoveLikeThisBefore_1980', 260)
+   hum <- blankend(hum, file == 'Madonna_OhFather_1990', 557)
+   hum <- blankend(hum, file == 'LedZeppelin_OverTheHillsAndFarAway_1973', 315)
+   hum <- blankend(hum, file == 'SpandauBallet_True_1983', 106)
+   hum <- blankend(hum, file == 'EricClapton_WillieAndTheHandJive_1974', 32)
+   hum <- blankend(hum, file == 'EarthWindAndFire_September_1979', 466)
+   hum <- blankend(hum, file == 'Heart_MagicMan_1976', 565)
+   hum <- blankend(hum, file == 'TheBeachBoys_Kokomo_1988', 514)
    hum
           
 }
 
+blankend <- function(hum, do, record) {
+   if (do) {
+      hum[Record >= record, NewTime := ifelse(is.na(Time), NA, Time)]
+      hum[Record >= record, Match := TRUE]
+   }
+   hum
+}
 
 chordsMatch <- function(old, new) {
    
    oldroot <- semits(old, simple = TRUE, Exclusive = 'kern')
    newroot <- semits(new, simple = TRUE, Exclusive = 'kern')
+   oldroot[old %in% c('r', 'N')] <- -10
+   newroot[new == 'N'] <- -10
+   
+   override <- grepl('_XXX', new)
    
    oldqual <- gsub('^.*:', '', old)
    newqual <- gsub('^.*:', '', new)
    
-   rest <- old == 'r' & new == 'N'
-   
-   rootmatch <- oldroot == newroot | rest
-   qualmatch <- oldqual == newqual | rest
+   rootmatch <- oldroot == newroot | override
+   qualmatch <- oldqual == newqual | (newroot == -10 & oldroot == -10) | override
    
    data.table(Rootmatch = rootmatch, Qualmatch = qualmatch, Match = qualmatch & rootmatch)
 }
@@ -107,7 +138,7 @@ generateFiles <- function(tsTable, filename, humfile) {
    filename <- paste0('Scripts/timestamps/', filename)
    
    if (tsTable[ , any(!Match, na.rm = TRUE)]) {
-      return(tsTable[!is.na(Time)])
+      return(tsTable[!is.na(NewTime)])
    } else {
       # fwrite(tsTable, file = paste0(filename, '.good'))
      newspine <- tsTable[ , ifelse(is.na(NewTime), Slate, NewTime)]
@@ -121,17 +152,22 @@ generateFiles <- function(tsTable, filename, humfile) {
 }
 
 i <- 1:nrow(sampleData)
+inspect <- FALSE
 sampleData[i, {
-   cat(FileName, ':\n', sep = '')
+   cat(i, ': ' , FileName, '\n', sep = '')
   humfile <- readLines(paste0('Humdrum/CompleteTranscriptions/', FileName, '.hum'))
   mirexfile <- readLines(paste0('OriginalData/MirexFiles/', stringr::str_pad(ID, width=4, pad = '0'), 'full.lab'))
   
-  tsTable <- generateTS(humfile, mirexfile)
+  tsTable <- generateTS(humfile, mirexfile, FileName)
   
   bad <- generateFiles(tsTable, FileName, humfile)
-  if (is.null(bad)) NULL else list(File = FileName, Bad = list(bad), N = sum(!bad$Match, na.rm = TRUE), P = mean(!bad$Match, na.rm = TRUE))
+  if (is.null(bad)) NULL else list(File = FileName, Bad = list(bad), ID = ID, N = sum(!bad$Match, na.rm = TRUE), P = mean(!bad$Match, na.rm = TRUE))
   
   
-}, by = i ] -> x
+}, by = i ] -> bad
 
 
+which <- bad[1]$Bad[[1]][, which(!Match)]
+print(bad[1]$ID)
+
+if (diff(range(which)) < 20) bad[1]$Bad[[1]][Filter(\(x) x >0, (min(which) - 10):(max(which) + 5))] |> print() else print(which)
