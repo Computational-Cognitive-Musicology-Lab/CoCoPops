78ac9c1d76bf9174a2ce5866461941997bcbdd2e
ncondits3 <ncondits3@gatech.edu>
2023-11-24 17:01:38 -0500

    Fixed a couple of minor errors in **harte, where old multistops were still there.)

--- Billboard/Resources/Scripts/ToHarm.R
+++ Billboard/Resources/Scripts/ToHarm.R
@@ -5,9 +5,9 @@ library(humdrumR)
 
 bb <- readHumdrum('~/Bridge/Research/Data/CoCoPops/Billboard/Data/.*hum')
 
-bb |> filter(Exclusive == 'harmony') |> harm(parseArgs = list(augment = 'A', diminish = 'd')) |> unfilter(complement = 'Token') -> bb
+bb |> filter(Exclusive == 'harmony') |> harm(parseArgs = list(augment = 'A', diminish = 'd'), inPlace = TRUE) |> unfilter(complement = 'Token') -> bbharm
 
-bb |> filter(Exclusive == 'harmony') |> 
+bbharm |> filter(Exclusive == 'harmony') |> 
   count(Harm=gsub('^[1-9][0-9]*\\.*|[0-9]*%[0-9]*', '', gsub(';', '', Harm)), 
         Harmony = gsub('^[1-9][0-9]*\\.*|[0-9]*%[0-9]*', '', gsub(';', '', Token)), 
         Key = Key) -> checktab
@@ -15,4 +15,9 @@ bb |> filter(Exclusive == 'harmony') |>
 
 as.data.table.count.frame(checktab) -> checktab
 
-# F G
+bb |> within(Harm2 <- {
+   inversion <- str_extract(Token[Exclusive == 'harte'], '/.*') |> str_extract('[1-7]') |> chartr(old = '1234567', new = 'aebfcgd')
+   Harm[Exclusive == 'harmony'] <- paste0(Harm[Exclusive == 'harmony'], inversion)
+   Harm
+   }
+  )
