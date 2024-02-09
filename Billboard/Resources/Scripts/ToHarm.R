ff41d832aedabfc6280e3dc3c9042ee1f4c7ab53
ncondits3 <ncondits3@gatech.edu>
2024-02-08 22:15:39 -0500

    Fixed chords and keys in IveGotLoveOnMyMind.

--- Billboard/Resources/Scripts/ToHarm.R
+++ Billboard/Resources/Scripts/ToHarm.R
@@ -8,7 +8,7 @@ bb <- readHumdrum('~/Bridge/Research/Data/CoCoPops/Billboard/Data/.*hum|.*harm')
 
 bb |> 
    filter(Exclusive == 'harmony') |> 
-   harm(parseArgs = list(augment = 'A', diminish = 'd'), inPlace = TRUE, natural = '#') |> 
+   harm(parseArgs = list(augment = 'A', diminish = 'd'), inPlace = TRUE, natural = '#', implicitSpecies = FALSE) |>  #implicitSpecies =TRUE, natural = '#'
    unfilter(complement = 'Token') -> bbharm
 
 bbharm |> filter(Exclusive == 'harmony') |> 
@@ -19,7 +19,7 @@ bbharm |> filter(Exclusive == 'harmony') |>
    unique() -> checktab
 
 checktab[, Root := tertianSet(Harm)@Root]
-setorder(checktab, Root)
+setorder(checktab, -Root)
 
 checktab <- checktab[!Filename %in% c("CheapTrick_DreamPolice_1979.hum",
                                       "PeachesAndHerb_ShakeYourGrooveThing_1978.harm",
@@ -29,7 +29,12 @@ checktab <- checktab[!Filename %in% c("CheapTrick_DreamPolice_1979.hum",
                                       "TheElectricPrunes_IHadTooMuchToDream_1967.hum",
                                       "DavidBowie_GoldenYears_1976.harm",
                                       "FreddieJackson_HaveYouEverLovedSomebody_1987.harm",
-                                      "BlueCheer_SummertimeBlues_1968.harm"
+                                      "BlueCheer_SummertimeBlues_1968.harm",
+                                      "TheRitchieFamily_TheBestDiscoInTown_1976.harm",
+                                      " NatalieCole_Unforgettable_1991.harm",
+                                      "TheSopwithCamel_HelloHello_1967.harm",
+                                      "ToddRundgren_ADreamGoesOnForever_1974.harm",
+                                      "WildCherry_PlayThatFunkyMusic_1976.harm"
                                       
                                       )]
 checktab[!is.na(Root)][1:40]
